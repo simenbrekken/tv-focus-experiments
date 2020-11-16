9f6a47ec690456001c9c3df436105cf6c2cabd7c
Alexander Pope <alex@pope-industries.com>
2020-11-16 14:49:49 +0100

    clean up

--- www/index.js
+++ www/index.js
@@ -1,92 +1,133 @@
-const activeSelector = '[data-active]';
-const containerSelector = '[data-container]';
-const focusableSelector = '[data-focusable]';
-
-const directionByKey = {
+// @ts-check
+const ACTIVE_SELECTOR = '[data-active]';
+const CONTAINER_SELECTOR = '[data-spatial-navigation-contain]';
+const FOCUSABLE_SELECTOR = '[tabindex], a[href], button:not([disabled])';
+const DIRECTION_BY_KEY = {
   ArrowDown: 'down',
   ArrowLeft: 'left',
   ArrowRight: 'right',
   ArrowUp: 'up',
 };
 
+document.addEventListener('keydown', handleKeyDown);
+
 /**
- * @param {DOMRect} rectA
- * @param {DOMRect} rectB
- * @param {string} direction
+ * @param {KeyboardEvent} event
  */
-function getSpatialPlanesForDirection(rectA, rectB, direction) {
-  switch (direction) {
-    case 'up':
-      return {
-        s1: [rectA.left, rectA.right],
-        s2: [rectB.left, rectB.right],
-        d: rectA.top - rectB.bottom,
-      };
-    case 'down':
-      return {
-        s1: [rectA.left, rectA.right],
-        s2: [rectB.left, rectB.right],
-        d: rectB.top - rectA.bottom,
-      };
-    case 'left':
-      return {
-        s1: [rectA.top, rectA.bottom],
-        s2: [rectB.top, rectB.bottom],
-        d: rectA.left - rectB.right,
-      };
-    case 'right':
-      return {
-        s1: [rectA.top, rectA.bottom],
-        s2: [rectB.top, rectB.bottom],
-        d: rectB.left - rectA.right,
-      };
+function handleKeyDown(event) {
+  const direction = DIRECTION_BY_KEY[event.key];
+
+  if (!direction) {
+    return;
+  }
+
+  const searchOrigin = /** @type {HTMLElement}*/ (event.target);
+  const container = /** @type {HTMLElement}*/ (searchOrigin.closest(
+    CONTAINER_SELECTOR
+  ));
+
+  if (!container) {
+    return;
+  }
+
+  const focusable = findFocusable(container, searchOrigin, direction, true);
+
+  if (focusable) {
+    focusable.focus();
   }
 }
 
 /**
- * @param {DOMRect} rectA
- * @param {DOMRect} rectB
+ * @param {HTMLElement} container
+ * @param {HTMLElement} searchOrigin
  * @param {string} direction
+ * @param {boolean} preferActive
  */
-function getDistanceInDirection(rectA, rectB, direction) {
-  const { s1, s2, d } = getSpatialPlanesForDirection(rectA, rectB, direction);
-  const isOverlaping =
-    (s1[0] >= s2[0] && s1[0] <= s2[1]) || (s1[1] >= s2[0] && s1[1] <= s2[1]);
+function findFocusable(container, searchOrigin, direction, preferActive) {
+  const candidates = /** @type {Array<HTMLElement>}*/ (Array.from(
+    container.querySelectorAll(FOCUSABLE_SELECTOR)
+  ));
+  const candidatesExcludingSearchOrigin = candidates.filter(
+    (candidate) => candidate !== searchOrigin
+  );
 
-  // If segments are overlapping, shortest distance is length of tangent (d)
-  if (isOverlaping) {
-    return d;
+  const searchOriginRect = searchOrigin.getBoundingClientRect();
+
+  const candidatesExcludingSourceOriginAndInvalidForDirection = candidatesExcludingSearchOrigin.filter(
+    (candidate) =>
+      isValidForDirection(
+        searchOriginRect,
+        candidate.getBoundingClientRect(),
+        direction
+      )
+  );
+
+  if (candidatesExcludingSourceOriginAndInvalidForDirection.length == 0) {
+    const closestContainerFromParent = container.parentElement.closest(
+      CONTAINER_SELECTOR
+    );
+    const containers = /** @type {Array<HTMLElement>}*/ (Array.from(
+      closestContainerFromParent.querySelectorAll(CONTAINER_SELECTOR)
+    ));
+
+    const containersExcludingCurrentContainer = containers.filter(
+      (candidate) => candidate !== container
+    );
+
+    const containerRect = container.getBoundingClientRect();
+
+    const containersExcludingCurrentContainerAndInvalidForDirection = containersExcludingCurrentContainer.filter(
+      (candidate) =>
+        isValidForDirection(
+          containerRect,
+          candidate.getBoundingClientRect(),
+          direction
+        )
+    );
+
+    const closestContainer = getClosestCandidate(
+      searchOrigin,
+      containersExcludingCurrentContainerAndInvalidForDirection,
+      direction
+    );
+
+    if (!closestContainer) {
+      return;
+    }
+
+    return findFocusable(closestContainer, searchOrigin, direction, true);
+  } else if (
+    candidatesExcludingSourceOriginAndInvalidForDirection.length === 1
+  ) {
+    return candidatesExcludingSourceOriginAndInvalidForDirection[0];
   }
 
-  // If A is right of B, measure distance between left-most A point and right-most B point
-  if (s1[0] > s2[1]) {
-    return Math.hypot(s1[0] - s2[1], d);
-  } else {
-    return Math.hypot(s1[1] - s2[0], d);
+  if (preferActive) {
+    const activeCandidate = candidatesExcludingSourceOriginAndInvalidForDirection.find(
+      (candidate) => candidate.matches(ACTIVE_SELECTOR)
+    );
+
+    if (activeCandidate) {
+      return activeCandidate;
+    }
   }
+
+  return getClosestCandidate(
+    searchOrigin,
+    candidatesExcludingSourceOriginAndInvalidForDirection,
+    direction
+  );
 }
 
 /**
- * @param {DOMRect} sourceRect
- * @param {DOMRect} targetRect
+ * Given an element and set of candidates, find the closest in a given direction
+ * @param {HTMLElement} searchOrigin
+ * @param {Array<HTMLElement>} candidates
  * @param {string} direction
+ * @returns {HTMLElement}
  */
-function isValidForDirection(sourceRect, targetRect, direction) {
-  switch (direction) {
-    case 'up':
-      return targetRect.bottom <= sourceRect.top;
-    case 'down':
-      return targetRect.top >= sourceRect.bottom;
-    case 'left':
-      return targetRect.right <= sourceRect.left;
-    case 'right':
-      return targetRect.left >= sourceRect.right;
-  }
-}
-
 function getClosestCandidate(searchOrigin, candidates, direction) {
   const sourceRect = searchOrigin.getBoundingClientRect();
-
   let closestCandidate;
   let minDistance = Number.POSITIVE_INFINITY;
 
@@ -123,100 +164,83 @@ function getClosestCandidate(searchOrigin, candidates, direction) {
   return closestCandidate;
 }
 
-function findFocusable(container, searchOrigin, direction, preferActive) {
-  const candidates = Array.from(container.querySelectorAll(focusableSelector));
-  const candidatesExcludingSearchOrigin = candidates.filter(
-    (candidate) => candidate !== searchOrigin
-  );
-
-  const searchOriginRect = searchOrigin.getBoundingClientRect();
-
-  const candidatesExcludingSourceOriginAndInvalidForDirection = candidatesExcludingSearchOrigin.filter(
-    (candidate) =>
-      isValidForDirection(
-        searchOriginRect,
-        candidate.getBoundingClientRect(),
-        direction
-      )
-  );
-
-  if (candidatesExcludingSourceOriginAndInvalidForDirection.length == 0) {
-    const closestContainerFromParent = container.parentElement.closest(
-      containerSelector
-    );
-    const containers = Array.from(
-      closestContainerFromParent.querySelectorAll(containerSelector)
-    );
-
-    const containersExcludingCurrentContainer = containers.filter(
-      (candidate) => candidate !== container
-    );
-
-    const containerRect = container.getBoundingClientRect();
-
-    const containersExcludingCurrentContainerAndInvalidForDirection = containersExcludingCurrentContainer.filter(
-      (candidate) =>
-        isValidForDirection(
-          containerRect,
-          candidate.getBoundingClientRect(),
-          direction
-        )
-    );
-
-    const closestContainer = getClosestCandidate(
-      searchOrigin,
-      containersExcludingCurrentContainerAndInvalidForDirection,
-      direction,
-      true
-    );
-
-    if (!closestContainer) {
-      return;
-    }
-
-    return findFocusable(closestContainer, searchOrigin, direction, true);
-  } else if (
-    candidatesExcludingSourceOriginAndInvalidForDirection.length === 1
-  ) {
-    return candidatesExcludingSourceOriginAndInvalidForDirection[0];
+/**
+ * @param {DOMRect} rectA
+ * @param {DOMRect} rectB
+ * @param {string} direction
+ */
+function isValidForDirection(rectA, rectB, direction) {
+  switch (direction) {
+    case 'up':
+      return rectB.bottom <= rectA.top;
+    case 'down':
+      return rectB.top >= rectA.bottom;
+    case 'left':
+      return rectB.right <= rectA.left;
+    case 'right':
+      return rectB.left >= rectA.right;
   }
+}
 
-  if (preferActive) {
-    const activeCandidate = candidatesExcludingSourceOriginAndInvalidForDirection.find(
-      (candidate) => candidate.matches(activeSelector)
-    );
-
-    if (activeCandidate) {
-      return activeCandidate;
-    }
-  }
-
-  return getClosestCandidate(
-    searchOrigin,
-    candidatesExcludingSourceOriginAndInvalidForDirection,
+/**
+ * @param {DOMRect} rectA
+ * @param {DOMRect} rectB
+ * @param {string} direction
+ */
+function getDistanceInDirection(rectA, rectB, direction) {
+  const { sideA, sideB, distance } = getRectSidesForDirection(
+    rectA,
+    rectB,
     direction
   );
-}
+  const isOverlaping =
+    (sideA[0] >= sideB[0] && sideA[0] <= sideB[1]) ||
+    (sideA[1] >= sideB[0] && sideA[1] <= sideB[1]);
 
-function handleKeyDown(event) {
-  const direction = directionByKey[event.key];
-
-  if (!direction) {
-    return;
+  // If sides are overlapping in one axis, shortest distance is length of tangent (d)
+  if (isOverlaping) {
+    return distance;
   }
 
-  const searchOrigin = event.target;
-  const container = searchOrigin.closest(containerSelector);
-
-  if (!container) {
-    return;
-  }
-
-  const focusable = findFocusable(container, searchOrigin, direction);
-
-  if (focusable) {
-    focusable.focus();
+  // If A side is right of B, measure distance between left-most A point and right-most B point
+  if (sideA[0] > sideB[1]) {
+    return Math.hypot(sideA[0] - sideB[1], distance);
+  } else {
+    return Math.hypot(sideA[1] - sideB[0], distance);
   }
 }
 
-document.addEventListener('keydown', handleKeyDown);
+/**
+ * Retrieve nearest sides of two rects based on a given direction
+ * @param {DOMRect} rectA
+ * @param {DOMRect} rectB
+ * @param {string} direction
+ */
+function getRectSidesForDirection(rectA, rectB, direction) {
+  switch (direction) {
+    case 'up':
+      return {
+        sideA: [rectA.left, rectA.right],
+        sideB: [rectB.left, rectB.right],
+        distance: rectA.top - rectB.bottom,
+      };
+    case 'down':
+      return {
+        sideA: [rectA.left, rectA.right],
+        sideB: [rectB.left, rectB.right],
+        distance: rectB.top - rectA.bottom,
+      };
+    case 'left':
+      return {
+        sideA: [rectA.top, rectA.bottom],
+        sideB: [rectB.top, rectB.bottom],
+        distance: rectA.left - rectB.right,
+      };
+    case 'right':
+      return {
+        sideA: [rectA.top, rectA.bottom],
+        sideB: [rectB.top, rectB.bottom],
+        distance: rectB.left - rectA.right,
+      };
+  }
+}
